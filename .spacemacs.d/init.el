fb93796330b352323a44b785261384c957d11c1f
Daniel Brady <daniel.brady@tapjoy.com>
2018-08-18 15:27:39 -0400

    Use the same indentation level across a variety of language modes

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -360,6 +360,22 @@ you should place your code here."
   (defvar magithub-clone-default-directory)
   (setq magithub-clone-default-directory "~/github")
 
+  ;; Setup indentation level for various modes
+  (defun set-indent (n)
+    ;; java/c/c++
+    (setq c-basic-offset n)
+    ;; web development
+    (setq coffee-tab-width n) ; coffeescript
+    (setq javascript-indent-level n) ; javascript-mode
+    (setq js-indent-level n) ; js-mode
+    (setq js2-basic-offset n) ; js2-mode, in latest js2-mode, it's alias of js-indent-level
+    (setq typescript-indent-level n) ; typescript
+    (setq web-mode-markup-indent-offset n) ; web-mode, html tag in html file
+    (setq web-mode-css-indent-offset n) ; web-mode, css in html file
+    (setq web-mode-code-indent-offset n) ; web-mode, js code in html file
+    (setq css-indent-offset n) ; css-mode
+   )
+  (set-indent 2)
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
