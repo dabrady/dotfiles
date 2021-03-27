ec5b8c558fbbd5269da03ed666719e7ce4c5e6ea
Daniel Brady <daniel.13rady@gmail.com>
2021-03-27 12:43:38 -1000

    Encapsulate some things in personal config layer

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -85,6 +85,7 @@ values."
      ;;==================== +custom
      my-spaceline
      bugfixes
+     myconf
      ;; (ambientheme :variables
      ;;              ambientheme-threshold 10
      ;;              ambientheme-light-theme 'leuven
@@ -409,20 +410,6 @@ you should place your code here."
   (setq magithub-clone-default-directory "~/github")
 
   ;; Setup indentation level for various modes
-  (defun set-indent (n)
-    ;; java/c/c++
-    (setq c-basic-offset n)
-    ;; web development
-    (setq coffee-tab-width n) ; coffeescript
-    (setq javascript-indent-level n) ; javascript-mode
-    (setq js-indent-level n) ; js-mode
-    (setq js2-basic-offset n) ; js2-mode, in latest js2-mode, it's alias of js-indent-level
-    (setq typescript-indent-level n) ; typescript
-    (setq web-mode-markup-indent-offset n) ; web-mode, html tag in html file
-    (setq web-mode-css-indent-offset n) ; web-mode, css in html file
-    (setq web-mode-code-indent-offset n) ; web-mode, js code in html file
-    (setq css-indent-offset n) ; css-mode
-   )
   (set-indent 2)
 
   ;; Enable prettier-js minor mode for various major modes (formats on save)
@@ -454,11 +441,6 @@ you should place your code here."
   ;; Fix auto-mode: I want '.m' files to be considered as Objective C source.
   (add-to-list 'auto-mode-alist '("\\.\\(?:mm\\|m\\)\\'" . objc-mode))
 
-  ;; For things like blog posts
-  (defun datestamp ()
-    "Inserts an ISO formatted date-time"
-    (interactive)
-    (insert (shell-command-to-string "echo -n `gdate -Iminutes`")))
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
---
+++ .spacemacs.d/layers/myconf/funcs.el
@@ -0,0 +1,38 @@
+;;; funcs.el --- Random functions
+
+;;; Commentary:
+;; This file contains a smattering of utility functions supporting my use of Spacemacs.
+
+;;; Code:
+
+(defun set-indent (n)
+  "Set indentation level to N for various major modes."
+  (setq c-basic-offset n) ; java/c/c++
+  (setq coffee-tab-width n) ; coffeescript
+  (setq javascript-indent-level n) ; javascript-mode
+  (setq js-indent-level n) ; js-mode
+  (setq js2-basic-offset n) ; js2-mode, in latest js2-mode, it's alias of js-indent-level
+  (setq typescript-indent-level n) ; typescript
+  (setq web-mode-markup-indent-offset n) ; web-mode, html tag in html file
+  (setq web-mode-css-indent-offset n) ; web-mode, css in html file
+  (setq web-mode-code-indent-offset n) ; web-mode, js code in html file
+  (setq css-indent-offset n) ; css-mode
+  (setq sh-basic-offset n) ; sh-mode
+  )
+
+;; For things like blog posts
+(defun datestamp ()
+  "Insert an ISO formatted date-time."
+  (interactive)
+  (insert (shell-command-to-string "echo -n `gdate -Iminutes`")))
+
+(defun change-jira (ticket-number)
+  "Change the current JIRA ticket context to TICKET-NUMBER."
+  (interactive "sChange JIRA context to: ")
+  (setenv "JIRA_CURRENT"
+          (shell-command-to-string
+           (format "source ~/.oh-my-zsh/custom/functions/jira.zsh && cj %s >/dev/null && printf $JIRA_CURRENT"
+                   ticket-number))))
+
+(provide 'funcs)
+;;; funcs.el ends here
